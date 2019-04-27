3d0814f4956c3cf13833a6098257f2c45fab6120
kec <keckchris@outlook.com>
2019-04-27 23:39:04 +0200

    Initial application generated by JHipster-6.0.0-beta.0

---
+++ glservice/src/test/java/de/iso/apps/cucumber/CucumberContextConfiguration.java
@@ -0,0 +1,20 @@
+package de.iso.apps.cucumber;
+
+import de.iso.apps.GlserviceApp;
+import cucumber.api.java.Before;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@WebAppConfiguration
+@ContextConfiguration(classes = GlserviceApp.class)
+public class CucumberContextConfiguration {
+
+    @Before
+    public void setup_cucumber_spring_context(){
+        // Dummy method so cucumber will recognize this class as glue
+        // and use its context configuration.
+    }
+
+}
