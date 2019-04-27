4168f727d0fa6c1db4728447d33e8435c666b977
kec <keckchris@outlook.com>
2019-04-27 23:27:39 +0200

    Initial application generated by JHipster-6.0.0-beta.0

---
+++ egateway/src/main/java/de/iso/apps/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package de.iso.apps.repository;
+
+import de.iso.apps.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
